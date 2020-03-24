764fc6651d6790584f28e89470f526f82d2fa673
StephenAustin <stephen.austin.au@gmail.com>
2020-03-25 02:19:41 +0800

    add vue plop template

--- plopfile.js
+++ plopfile.js
@@ -1,5 +1,14 @@
-const ctrl = require('./egg-rest/prompt')
+const egg_rest = require('./egg-rest/prompt')
+const vue_rest = require('./vue-rest/prompt')
+
+const vue_view = require('./vue-view/prompt')
+const vue_component = require('./vue-component/prompt')
+const vue_store = require('./vue-store/prompt.js')
 
 module.exports = function(plop) {
-  plop.setGenerator('ctrl', ctrl)
+  plop.setGenerator('egg-rest', egg_rest )
+  plop.setGenerator('vue-rest', vue_rest )
+  plop.setGenerator('vue-view', vue_view )
+  plop.setGenerator('vue-component', vue_component )
+  plop.setGenerator('vue-store', vue_store )
 }
