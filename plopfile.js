1bb31015e49fc242b90546c3c25b22264535734b
zhangzhichao <zhangzhichao@pxjy.com>
2020-08-05 11:23:23 +0800

    feat:egg-resources-rest模板

--- plopfile.js
+++ plopfile.js
@@ -1,4 +1,5 @@
 const egg_rest = require('./egg-rest/prompt')
+const egg_resources_rest = require('./egg-resources-rest/prompt')
 const vue_rest = require('./vue-rest/prompt')
 
 const vue_view = require('./vue-view/prompt')
@@ -6,6 +7,7 @@ const vue_component = require('./vue-component/prompt')
 const vue_store = require('./vue-store/prompt.js')
 
 module.exports = function(plop) {
+  plop.setGenerator('egg-resources-rest', egg_resources_rest )
   plop.setGenerator('egg-rest', egg_rest )
   plop.setGenerator('vue-rest', vue_rest )
   plop.setGenerator('vue-view', vue_view )
